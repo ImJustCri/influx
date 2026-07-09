ab463c6fc9ca3ffe774c16653c4b704da875ed8a
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-07-09 10:55:35 +0200

    Redesign home page UI with budget and expenses

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:influx/widgets/home_app_bar.dart';
+import 'package:influx/widgets/home/home_app_bar.dart';
 import 'package:influx/widgets/page_padding.dart';
-import '../widgets/app_container.dart';
+
+import '../models/expense_data.dart';
+import '../widgets/charts/simple_trend_chart.dart';
+import '../widgets/expenses/expense_item.dart';
+import '../widgets/home/budget_card.dart';
+import '../widgets/home/recent_expenses_section.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  static const List<ExpenseData> recentExpenses = [
+    ExpenseData(
+      type: ExpenseType.electronics,
+      title: 'Undertale',
+      amount: '2,5',
+    ),
+    ExpenseData(
+      type: ExpenseType.clothing,
+      title: 'JD Sport',
+      amount: '50',
+    ),
+    ExpenseData(
+      type: ExpenseType.food,
+      title: 'Carrefour',
+      amount: '18,30',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const HomeAppBar(),
-        PagePadding(
-          child: AppContainer(
-            child: const Text('Hello', style: TextStyle(color: Colors.white)),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height + 150,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const HomeAppBar(),
+              PagePadding(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const BudgetCard(),
+                    const SizedBox(height: 24),
+                    SimpleTrendChart(
+                      startValue: 1500,
+                      endValue: 1600,
+                    ),
+                    const SizedBox(height: 24),
+                    RecentExpensesSection(expenses: recentExpenses),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
