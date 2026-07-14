55fd19e2ab33b0fcdc96c6c995fa1d9a6ee26a48
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-07-14 17:07:10 +0200

    wrap app in ProviderScope

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:influx/services/auth_service.dart';
 import 'package:influx/pages/account/profile.dart';
-import 'package:influx/pages/main_shell_screen.dart';
 import 'package:influx/theme.dart';
 import 'package:influx/widgets/global_background.dart';
 
@@ -10,24 +10,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'INFLUX',
-      theme: darkTheme,
-      builder: (context, child) {
-        return Stack(
-          children: [
-            const Positioned.fill(child: GlobalBackground()),
-            ?child,
-          ],
-        );
-      },
-      home: const AuthService(),
-      routes: {
-        '/profile': (context) => ProfilePage(
-          userUuid: "UUID Placeholder",
-          budget: 500,
-        ),
-      },
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'INFLUX',
+        theme: darkTheme,
+        builder: (context, child) {
+          return Stack(
+            children: [
+              const Positioned.fill(child: GlobalBackground()),
+              child!,
+            ],
+          );
+        },
+        home: const AuthService(),
+        routes: {
+          '/profile': (context) => ProfilePage(
+            budget: 500,
+          ),
+        },
+      ),
     );
   }
 }
