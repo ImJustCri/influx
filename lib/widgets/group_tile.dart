87db65253b671c7638859cd03bc3fa1079beb04b
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-07-31 13:00:38 +0200

    improve tile ui and add owner indicator

--- lib/widgets/group_tile.dart
+++ lib/widgets/group_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
 import '../theme.dart';
 
 class GroupTile extends StatelessWidget {
@@ -6,7 +7,7 @@ class GroupTile extends StatelessWidget {
   final String title;
   final int members;
   final VoidCallback onTap;
-  final bool? isLast;
+  final bool isUserGroupOwner;
 
   const GroupTile({
     super.key,
@@ -14,54 +15,65 @@ class GroupTile extends StatelessWidget {
     required this.title,
     required this.onTap,
     required this.members,
-    this.isLast = false,
+    required this.isUserGroupOwner,
   });
 
   @override
   Widget build(BuildContext context) {
+    // Dynamic text based on count
+    final String memberText = members == 1 ? '$members membro' : '$members membri';
+
     return Column(
       children: [
-        InkWell(
-          onTap: onTap,
-          borderRadius: BorderRadius.circular(32),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 24,
-                  backgroundColor: AppColors.btnBackground,
-                  child: Icon(icon, color: AppColors.white, size: 24),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: AppTypography.containerTitle,
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        "$members membri",
-                        style: AppTypography.containerBody,
-                      ),
-                    ],
+        Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            borderRadius: BorderRadius.circular(32),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          title,
+                          style: AppTypography.containerTitle,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          memberText,
+                          style: AppTypography.containerBody,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(width: 8),
+                  isUserGroupOwner ? CircleAvatar(
+                    backgroundColor: Colors.purple,
+                    child: Icon(
+                      LucideIcons.crown,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  )
+                      : CircleAvatar(
+                    backgroundColor: Colors.indigoAccent,
+                    child: Icon(
+                      LucideIcons.user,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        if (!isLast!)
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Divider(
-              height: 1,
-              color: AppColors.inputBorder,
-            ),
-          ),
       ],
     );
   }
