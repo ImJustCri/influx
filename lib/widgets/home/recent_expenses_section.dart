18a9f8cc54168aceabfd8d021f0f2e546b84408a
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-04 19:30:06 +0200

    Show empty state in recent expenses

--- lib/widgets/home/recent_expenses_section.dart
+++ lib/widgets/home/recent_expenses_section.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:influx/widgets/app_container.dart';
 
 import '../../theme.dart';
 import '../../models/expense_data.dart';
@@ -17,18 +19,35 @@ class RecentExpensesSection extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Ultime spese", style: AppTypography.containerBody),
-        ...expenses.map(
-              (expense) => ExpenseItem(
-                categoryColor: expense.categoryColor,
-                categoryIcon: expense.categoryIcon,
-                categoryName: expense.categoryName,
-                title: expense.title,
-                amount: expense.amount,
-                purchaseDate: expense.purchaseDate,
-                description: expense.description,
+
+        if (expenses.isEmpty)
+          AppContainer(
+            padding: EdgeInsetsGeometry.all(48),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Icon(LucideIcons.book_search, size: 32),
+                SizedBox(height: 24),
+                Text("Niente da vedere quì", style: AppTypography.containerTitle, textAlign: TextAlign.center),
+                SizedBox(height: 4),
+                Text("Che ne dici di aggiungere una nuova spesa?", style: AppTypography.containerBody, textAlign: TextAlign.center),
+              ],
+            ),
+          )
+        else
+          Text("Ultime spese", style: AppTypography.containerBody),
+            ...expenses.map(
+                (expense) => ExpenseItem(
+                  categoryColor: expense.categoryColor,
+                  categoryIcon: expense.categoryIcon,
+                  categoryName: expense.categoryName,
+                  title: expense.title,
+                  amount: expense.amount,
+                  purchaseDate: expense.purchaseDate,
+                  description: expense.description,
+                  groupName: expense.groupName,
+            ),
           ),
-        ),
       ],
     );
   }
