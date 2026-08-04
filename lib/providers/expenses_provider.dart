a32a86129850bc6ace7fd6f0c58a21c36d02493a
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-04 19:28:50 +0200

    Include group relation when fetching expenses

--- lib/providers/expenses_provider.dart
+++ lib/providers/expenses_provider.dart
@@ -7,7 +7,7 @@ final fetchExpenses= FutureProvider<List<ExpenseData>>((ref) async {
 
   final user_id= await Supabase.instance.client.auth.currentUser!.id;
 
-  final response= await  Supabase.instance.client.from('expense').select('*, category(*)').eq('profile_id',user_id);
+  final response= await  Supabase.instance.client.from('expense').select('*, category(*), group(*)').eq('profile_id',user_id);
 
   final result= response.map((item)=>ExpenseData.convertJson(item)).toList();
 
