abfb0d865c6279a7198e092b031eb3dbc0402f66
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-04 22:26:07 +0200

    Add RootApp with ProviderScope and restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
-import 'my_app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'my_app.dart';
 
 Future<void> main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load();
 
   await Supabase.initialize(
-      url: dotenv.env['SUPABASE_URL']!,
-      publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_PUBLISHABLE_KEY']!,
   );
 
-  runApp(const MyApp());
+  runApp(const RootApp());
+}
+
+class RootApp extends StatefulWidget {
+  const RootApp({super.key});
+
+  /// restart context to delete cached state
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RootAppState>()?.restart();
+  }
+
+  @override
+  State<RootApp> createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  Key _key = UniqueKey();
+
+  void restart() {
+    setState(() {
+      _key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      key: _key,
+      child: const MyApp(),
+    );
+  }
 }
