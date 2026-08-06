f6f6b6caed543a45801c23f3c153044ac48ec265
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-06 10:25:03 +0200

    Add fetchLatestExpenses

--- lib/providers/expenses_provider.dart
+++ lib/providers/expenses_provider.dart
@@ -2,20 +2,32 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../models/expense_data.dart';
 
+/// Fetch latest three expenses
+final fetchLatestExpenses = FutureProvider.family<List<ExpenseData>, int>((ref, limit) async {
+  final userId = Supabase.instance.client.auth.currentUser!.id;
 
-final fetchExpenses= FutureProvider<List<ExpenseData>>((ref) async {
+  final response = await Supabase.instance.client
+      .from('expense')
+      .select('*, category(*), group(*)')
+      .eq('profile_id', userId)
+      .order('created_at', ascending: false)
+      .limit(limit);
 
-  final user_id= await Supabase.instance.client.auth.currentUser!.id;
-
-  final response= await  Supabase.instance.client.from('expense').select('*, category(*), group(*)').eq('profile_id',user_id);
-
-  final result= response.map((item)=>ExpenseData.convertJson(item)).toList();
-
-  return result;
+  return response.map((item) => ExpenseData.convertJson(item)).toList();
 });
 
+/// Fetch all expenses
+final fetchExpenses = FutureProvider<List<ExpenseData>>((ref) async {
+  final userId = Supabase.instance.client.auth.currentUser!.id;
 
+  final response = await Supabase.instance.client
+      .from('expense')
+      .select('*, category(*), group(*)')
+      .eq('profile_id', userId)
+      .order('created_at', ascending: false);
 
+  return response.map((item) => ExpenseData.convertJson(item)).toList();
+});
 
 
 
