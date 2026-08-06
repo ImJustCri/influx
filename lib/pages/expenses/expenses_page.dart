8f9013025cd3b7dc00c31460c9e3cd625741e7a9
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-06 11:31:02 +0200

    Add 'View all expenses' tile and UI tweaks

--- lib/pages/expenses/expenses_page.dart
+++ lib/pages/expenses/expenses_page.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:influx/widgets/expenses/all_expenses_page.dart';
 
 import 'package:influx/widgets/page_padding.dart';
+import 'package:influx/widgets/settings_tile.dart';
 import '../../models/expense_data.dart';
 import '../../theme.dart';
 import 'package:influx/providers/expenses_provider.dart';
 import '../../widgets/app_container.dart';
 import '../../widgets/charts/simple_trend_chart.dart';
 import '../../widgets/expenses/expense_category_bar.dart';
-
+import 'category_expenses_page.dart';
 
 class ExpensesPage extends ConsumerStatefulWidget {
   const ExpensesPage({super.key});
@@ -18,12 +20,9 @@ class ExpensesPage extends ConsumerStatefulWidget {
   ConsumerState<ExpensesPage> createState() => ExpensensState();
 }
 
-
 class ExpensensState extends ConsumerState<ExpensesPage> {
-
   @override
   Widget build(BuildContext context) {
-
     final expensesAsync = ref.watch(fetchExpenses);
 
     return Scaffold(
@@ -40,36 +39,35 @@ class ExpensensState extends ConsumerState<ExpensesPage> {
         elevation: 0,
         foregroundColor: Colors.white,
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          bottom: 124,
+        ),
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: PagePadding(
+          child: expensesAsync.when(
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            error: (error, stack) => Center(
+              child: Text(error.toString()),
+            ),
+            data: (expenses) {
+              final double totalSpent = expenses.fold(
+                0.0,
+                    (sum, item) => sum + item.numericAmount,
+              );
 
-      body: PagePadding(
-        child: expensesAsync.when(
-          loading: () => const Center(
-            child: CircularProgressIndicator(),
-          ),
+              // Group expenses by categories
+              final Map<String, List<ExpenseData>> categories = {};
+              for (final expense in expenses) {
+                categories.putIfAbsent(
+                  expense.categoryName,
+                      () => [],
+                ).add(expense);
+              }
 
-          error: (error, stack) => Center(
-            child: Text(error.toString()),
-          ),
-
-          data: (expenses) {
-            final double totalSpent = expenses.fold(0.0,
-                  (sum, item) => sum + item.numericAmount,
-            );
-
-            // Raggruppa le spese per categoria
-            final Map<String, List<ExpenseData>> categories = {};
-
-            if (expenses.isEmpty) {
-
-            }
-            for (final expense in expenses) {
-              categories.putIfAbsent(
-                expense.categoryName, () => [],
-              ).add(expense);
-            }
-
-            return SingleChildScrollView(
-              child: Column(
+              return Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   SimpleTrendChart(
@@ -79,52 +77,70 @@ class ExpensensState extends ConsumerState<ExpensesPage> {
                   ),
 
                   if (expenses.isEmpty) ...[
-                    SizedBox(height: 24),
+                    const SizedBox(height: 24),
                     AppContainer(
-                      padding: EdgeInsetsGeometry.all(48),
+                      padding: const EdgeInsets.all(48),
                       width: double.infinity,
                       child: Column(
                         children: [
-                          Icon(LucideIcons.book_search, size: 32),
-                          SizedBox(height: 24),
-                          Text("Niente da vedere quì", style: AppTypography.containerTitle, textAlign: TextAlign.center),
-                          SizedBox(height: 4),
-                          Text("Che ne dici di aggiungere una nuova spesa?", style: AppTypography.containerBody, textAlign: TextAlign.center),
+                          const Icon(LucideIcons.book_search, size: 32),
+                          const SizedBox(height: 24),
+                          Text(
+                            "Niente da vedere qui",
+                            style: AppTypography.containerTitle,
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 4),
+                          Text(
+                            "Che ne dici di aggiungere una nuova spesa?",
+                            style: AppTypography.containerBody,
+                            textAlign: TextAlign.center,
+                          ),
                         ],
                       ),
                     ),
                   ],
 
-                  SizedBox(height: 24),
-                  Column(
-                    spacing: 16,
-                    children: [
-                      ...categories.entries.map(
+                  if (expenses.isNotEmpty) ...[
+                    const SizedBox(height: 24),
+                    SettingsTile(
+                        icon: LucideIcons.list_collapse,
+                        title: "Vedi tutte le spese del periodo",
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => AllExpensesPage()));
+                        }
+                    ),
+
+                    const SizedBox(height: 24),
+                    Column(
+                      spacing: 16,
+                      children: categories.entries.map(
                             (entry) {
-                              final categoryExpenses = entry.value;
-                              final categoryInfo = categoryExpenses.first;
-                              final double categorySpent = categoryExpenses.fold(0.0,
-                                    (sum, item) =>
-                                sum + item.numericAmount,
-                              );
+                          final categoryExpenses = entry.value;
+                          final categoryInfo = categoryExpenses.first;
+                          final double categorySpent = categoryExpenses.fold(
+                            0.0,
+                                (sum, item) => sum + item.numericAmount,
+                          );
 
                           return ExpenseCategoryBar(
                             categoryName: categoryInfo.categoryName,
                             categoryIcon: categoryInfo.categoryIcon,
                             categoryColor: categoryInfo.categoryColor,
                             amount: categorySpent,
-                            percentage: totalSpent == 0 ? 0 : categorySpent / totalSpent,
+                            percentage: totalSpent == 0
+                                ? 0
+                                : categorySpent / totalSpent,
+                            categoryId: categoryInfo.categoryId,
                           );
-
                         },
-                      ),
-
-                    ],
-                  ),
+                      ).toList(),
+                    ),
+                  ],
                 ],
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
