8428e113dd5829d4e01f859d3dc9be514146d964
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-06 11:30:06 +0200

    Add Italian locale date formatting support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'my_app.dart';
 
@@ -8,6 +9,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await dotenv.load();
+
+  await initializeDateFormatting('it_IT');
 
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
