019545d9fb74aa482c7a5942fb0667999d89e608
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-07 11:51:10 +0200

    Pass member info to AllExpensesPage

--- lib/widgets/expenses/all_expenses_page.dart
+++ lib/widgets/expenses/all_expenses_page.dart
@@ -5,11 +5,15 @@ import 'package:intl/intl.dart';
 import 'package:influx/theme.dart';
 import 'package:influx/widgets/page_padding.dart';
 import '../../models/expense_data.dart';
+import '../../models/group_member.dart';
 import '../../providers/expenses_provider.dart';
 import '../../widgets/expenses/expense_item.dart';
 
 class AllExpensesPage extends ConsumerWidget {
-  const AllExpensesPage({super.key});
+  final List<GroupMember>? members;
+  final String? groupId;
+
+  const AllExpensesPage({super.key, this.members, this.groupId});
 
   /// Helper function to group expenses by formatted date string
   Map<String, List<ExpenseData>> _groupExpensesByDate(List<ExpenseData> expenses) {
@@ -17,7 +21,6 @@ class AllExpensesPage extends ConsumerWidget {
 
     for (final expense in expenses) {
       final DateTime date = expense.purchaseDate;
-
       final String dateHeader = DateFormat('dd MMMM yyyy', 'it_IT').format(date);
 
       grouped.putIfAbsent(dateHeader, () => []).add(expense);
@@ -26,9 +29,21 @@ class AllExpensesPage extends ConsumerWidget {
     return grouped;
   }
 
+  /// Helper function to find a member by profileId
+  GroupMember? _findMember(String? profileId) {
+    if (members == null || profileId == null) return null;
+
+    for (final member in members!) {
+      if (member.id == profileId) {
+        return member;
+      }
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final expensesAsync = ref.watch(fetchExpenses);
+    final expensesAsync = groupId != null ? ref.watch(fetchExpensesByGroupProvider(groupId!)) : ref.watch(fetchExpenses);
 
     return Scaffold(
       appBar: AppBar(
@@ -74,18 +89,27 @@ class AllExpensesPage extends ConsumerWidget {
                               ),
 
                               ...dayExpenses.map(
-                                    (expense) => ExpenseItem(
-                                  categoryColor: expense.categoryColor,
-                                  categoryIcon: expense.categoryIcon,
-                                  categoryName: expense.categoryName,
-                                  title: expense.title,
-                                  amount: expense.amount,
-                                  purchaseDate: expense.purchaseDate,
-                                  description: expense.description,
-                                  groupName: expense.groupName,
-                                  expenseId: expense.id,
-                                  categoryId: expense.categoryId,
-                                ),
+                                    (expense) {
+                                  // Look up the member using the expense's profileId
+                                  final matchingMember = _findMember(expense.profileId);
+
+                                  return ExpenseItem(
+                                    categoryColor: expense.categoryColor,
+                                    categoryIcon: expense.categoryIcon,
+                                    categoryName: expense.categoryName,
+                                    title: expense.title,
+                                    amount: expense.amount,
+                                    purchaseDate: expense.purchaseDate,
+                                    description: expense.description,
+                                    groupName: expense.groupName,
+                                    expenseId: expense.id,
+                                    categoryId: expense.categoryId,
+
+                                    // Pass properties safely using null-aware operators
+                                    userName: matchingMember?.name,
+                                    userPfp: matchingMember?.avatarImageUrl,
+                                  );
+                                },
                               ),
                             ],
                           ),
