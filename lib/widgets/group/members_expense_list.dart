c0e58e338372bd8cd136d574bdd968eba86a980c
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-07 11:50:27 +0200

    Fetch per-member expenses via provider

--- lib/widgets/group/members_expense_list.dart
+++ lib/widgets/group/members_expense_list.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../providers/profile_group_expense_sum_profile.dart';
 import 'group_member_expense_tile.dart';
 import '../../models/group_member.dart';
 
 class MembersExpenseList extends StatelessWidget {
   final List<GroupMember> members;
+  final String groupId;
+  final double perCapitaBudget;
 
   const MembersExpenseList({
     super.key,
     required this.members,
+    required this.groupId,
+    required this.perCapitaBudget,
   });
 
   @override
@@ -19,13 +25,27 @@ class MembersExpenseList extends StatelessWidget {
       separatorBuilder: (context, index) => const SizedBox(height: 0),
       itemBuilder: (context, index) {
         final member = members[index];
-        return GroupMemberExpenseTile(
-          memberName: member.name,
-          amount: member.amount,
-          avatarImageUrl: member.avatarImageUrl,
-          progressValue: member.progressValue,
-          backgroundColor: member.backgroundColor,
-          valueColor: member.valueColor,
+
+        return Consumer(
+          builder: (context, ref, child) {
+            final expenseAsync = ref.watch(
+              profileGroupExpenseSumProvider((
+              profileId: member.id,
+              groupId: groupId,
+              )),
+            );
+
+            final double? totalExpense = expenseAsync.value;
+
+            return GroupMemberExpenseTile(
+              memberName: member.name,
+              amount: totalExpense ?? 0,
+              avatarImageUrl: member.avatarImageUrl,
+              progressValue: totalExpense != null ? totalExpense / perCapitaBudget : 0,
+              backgroundColor: member.backgroundColor,
+              valueColor: member.valueColor,
+            );
+          },
         );
       },
     );
