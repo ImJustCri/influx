01a3a5235b0c8c3999198a17323a3604eb7d9ded
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-07 12:32:17 +0200

    pass groupName, memberId and groupId

--- lib/widgets/group/members_expense_list.dart
+++ lib/widgets/group/members_expense_list.dart
@@ -8,12 +8,14 @@ class MembersExpenseList extends StatelessWidget {
   final List<GroupMember> members;
   final String groupId;
   final double perCapitaBudget;
+  final String groupName;
 
   const MembersExpenseList({
     super.key,
     required this.members,
     required this.groupId,
     required this.perCapitaBudget,
+    required this.groupName,
   });
 
   @override
@@ -38,12 +40,15 @@ class MembersExpenseList extends StatelessWidget {
             final double? totalExpense = expenseAsync.value;
 
             return GroupMemberExpenseTile(
+              groupId: groupId,
+              memberId: member.id,
               memberName: member.name,
               amount: totalExpense ?? 0,
               avatarImageUrl: member.avatarImageUrl,
               progressValue: totalExpense != null ? totalExpense / perCapitaBudget : 0,
               backgroundColor: member.backgroundColor,
               valueColor: member.valueColor,
+              groupName: groupName,
             );
           },
         );
