2c020b8162526e8b78dbbca9ce4b975bb164790e
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-08 15:38:09 +0200

    Clean authservice

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,46 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:influx/pages/auth/login.dart';
-import 'package:influx/pages/main_shell_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import '../pages/periods/period_ended_page.dart';
-import '../providers/user_period_providers.dart';
-
-
-class AuthService extends ConsumerWidget {
-  const AuthService({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final session = Supabase.instance.client.auth.currentSession;
-
-    if (session == null) {
-      return const LoginPage();
-    }
-
-    final activePeriodAsync = ref.watch(activeUserPeriodProvider);
-
-    return activePeriodAsync.when(
-      data: (period) {
-        if (period == null) {
-          return const PeriodEndedPage();
-        }
-        return const MainShellScreen();
-      },
-      loading: () => const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      ),
-      error: (error, stack) => Scaffold(
-        body: Center(
-          child: Text('Errore nel caricamento del periodo: $error'),
-        ),
-      ),
-    );
-  }
-
+class AuthService {
   Future<String?> loginWithEmail(String email_) async {
     try {
       await Supabase.instance.client.auth.signInWithOtp(
