7f1d3548558ff088a5e83b0c7111fdee23b15358
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-08 22:12:07 +0200

    Disallow non-owner from removing admin members

--- lib/pages/groups/settings/group_member_edit.dart
+++ lib/pages/groups/settings/group_member_edit.dart
@@ -11,7 +11,8 @@ import '../../../widgets/settings_tile.dart';
 class GroupMemberEdit extends StatelessWidget {
   final String groupId;
   final GroupMember member;
-  const GroupMemberEdit({super.key, required this.member, required this.groupId});
+  final bool isCurrentUserGroupOwner;
+  const GroupMemberEdit({super.key, required this.member, required this.groupId, required this.isCurrentUserGroupOwner});
 
   /// Function to remove member from database
   Future<void> _removeMember(BuildContext context) async {
@@ -45,6 +46,13 @@ class GroupMemberEdit extends StatelessWidget {
           .eq('profile_id', member.id);
 
       if (!context.mounted) return;
+
+      if (member.isAdmin && !isCurrentUserGroupOwner) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Che fai, provi a rimuovere un altro admin?")),
+        );
+        return;
+      }
 
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Membro rimosso con successo.")),
@@ -105,7 +113,7 @@ class GroupMemberEdit extends StatelessWidget {
                 icon: LucideIcons.x,
                 title: "Rimuovi dal gruppo",
                 onTap: () => _removeMember(context),
-              )
+              ),
             ],
           ),
         ),
