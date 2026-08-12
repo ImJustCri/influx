b16925262bb75172be77483a72ce6232ce6a57b2
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-12 14:21:14 +0200

    remove budget from profile model

--- lib/models/profile.dart
+++ lib/models/profile.dart
@@ -2,13 +2,11 @@ class Profile {
   final String id;
   final String? fullName;
   final String? avatarUrl;
-  final double budget;
 
   Profile({
     required this.id,
     this.fullName,
     this.avatarUrl,
-    required this.budget,
   });
 
   // json to profile object
@@ -17,7 +15,6 @@ class Profile {
       id: json['id'] as String,
       fullName: json['full_name'] as String?,
       avatarUrl: json['avatar_url'] as String?,
-      budget: (json['budget_personale'] as num?)?.toDouble() ?? 0.0,
     );
   }
 }
