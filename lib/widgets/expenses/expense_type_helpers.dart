097ed8fdc8b328cbf10faefefb19f0343793079e
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-08-14 13:15:31 +0200

    Update expense_type_helpers.dart

--- lib/widgets/expenses/expense_type_helpers.dart
+++ lib/widgets/expenses/expense_type_helpers.dart
@@ -1,28 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
 
-  IconData getIconFromName(String icon) {
-    switch (icon) {
-      case 'pill':
-        return LucideIcons.pill;
-      case 'shopping_basket':
-        return LucideIcons.shopping_basket;
-      case 'laptop':
-        return Icons.laptop;
-      case 'smartphone':
-        return LucideIcons.smartphone;
-      case 'shirt':
-        return LucideIcons.shirt;
-      case 'party_popper':
-        return LucideIcons.party_popper;
-      case 'fuel':
-        return LucideIcons.fuel;
-      case 'banknote':
-        return LucideIcons.banknote;
-      case 'bone':
-        return LucideIcons.bone;
-        default:
-        return LucideIcons.a_large_small;
-    }
-  }
+IconData getIconFromName(String icon) {
+  switch (icon) {
+  // Original Categories
+    case 'pill':
+      return LucideIcons.pill;
+    case 'shopping_basket':
+      return LucideIcons.shopping_basket;
+    case 'laptop':
+      return LucideIcons.laptop;
+    case 'smartphone':
+      return LucideIcons.smartphone;
+    case 'shirt':
+      return LucideIcons.shirt;
+    case 'party_popper':
+      return LucideIcons.party_popper;
+    case 'fuel':
+      return LucideIcons.fuel;
+    case 'banknote':
+      return LucideIcons.banknote;
+    case 'bone':
+      return LucideIcons.bone;
 
+  // New Categories
+    case 'utensils':
+      return LucideIcons.utensils;
+    case 'home':
+      return LucideIcons.house;
+    case 'bus':
+      return LucideIcons.bus;
+    case 'activity':
+      return LucideIcons.activity;
+    case 'sparkles':
+      return LucideIcons.sparkles;
+    case 'graduation_cap':
+      return LucideIcons.graduation_cap;
+    case 'trending_up':
+      return LucideIcons.trending_up;
+    case 'receipt':
+      return LucideIcons.receipt;
+    case 'plane':
+      return LucideIcons.plane;
+    case 'gift':
+      return LucideIcons.gift;
+
+    default:
+      return LucideIcons.circle;
+  }
+}
