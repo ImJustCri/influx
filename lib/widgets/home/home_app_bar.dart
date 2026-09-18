097f4de5bb70cfdf479a00832cccbcb208dc8c62
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-09-18 12:50:29 +0200

    fetch profile provider and show pfp instead of generic icon

--- lib/widgets/home/home_app_bar.dart
+++ lib/widgets/home/home_app_bar.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lucide/flutter_lucide.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../pages/notifications_page.dart';
+import '../../providers/profile_provider.dart';
 import '../../theme.dart';
 
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+class HomeAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userProfileAsyncValue = ref.watch(profileProvider);
+
     return AppBar(
       toolbarHeight: 72,
       scrolledUnderElevation: 0,
@@ -26,23 +30,46 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
         ),
       ),
       actions: [
-        // IconButton(
-        //   icon: const Icon(LucideIcons.bell),
-        //   tooltip: 'Notifiche',
-        //   onPressed: () {
-        //     Navigator.push(
-        //       context, MaterialPageRoute(builder: (context) => NotificationsPage())
-        //     );
-        //   },
-        // ),
         IconButton(
-          icon: const Icon(LucideIcons.circle_user, color: AppColors.white,),
           tooltip: 'Il tuo account',
           onPressed: () {
             Navigator.pushNamed(context, '/profile');
           },
+          icon: userProfileAsyncValue.maybeWhen(
+            data: (profile) {
+              final avatarUrl = profile?.avatarUrl;
+              if (avatarUrl != null && avatarUrl.isNotEmpty) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(1.5),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: AppColors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: CircleAvatar(
+                      radius: 16,
+                      backgroundColor: AppColors.backgroundAccent,
+                      backgroundImage: NetworkImage(avatarUrl),
+                    ),
+                  ),
+                );
+              }
+              return const Icon(
+                LucideIcons.circle_user,
+                color: AppColors.white,
+              );
+            },
+            orElse: () => const Icon(
+              LucideIcons.circle_user,
+              color: AppColors.white,
+            ),
+          ),
         ),
-        const SizedBox(width: 16),
+        const SizedBox(width: 8),
       ],
     );
   }
