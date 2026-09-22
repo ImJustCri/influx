a445857e0655b500d1b50c12325a6c2f02e97fef
ImJustCri <160153780+ImJustCri@users.noreply.github.com>
2026-09-22 16:12:15 +0200

    Add animated status container styling

--- lib/widgets/status_container.dart
+++ lib/widgets/status_container.dart
@@ -4,40 +4,94 @@ import 'package:influx/widgets/app_container.dart';
 
 import '../../theme.dart';
 
-class StatusContainer extends StatelessWidget {
+class StatusContainer extends StatefulWidget {
   final IconData icon;
   final String title;
   final String description;
   final double iconSize;
+  final Color? iconColor;
+  final Color? backgroundColor;
 
   const StatusContainer({
     super.key,
-    this.icon = LucideIcons.loader,
+    this.icon = LucideIcons.loader_pinwheel,
     this.title = "Caricamento...",
     this.description = "Nel frattempo che aspetti, caffè?",
     this.iconSize = 32,
+    this.iconColor,
+    this.backgroundColor,
   });
 
   @override
+  State<StatusContainer> createState() => _StatusContainerState();
+}
+
+class _StatusContainerState extends State<StatusContainer>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  bool get _shouldRotate => widget.icon == LucideIcons.loader_pinwheel;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    if (_shouldRotate) {
+      _controller.repeat();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant StatusContainer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (_shouldRotate && !_controller.isAnimating) {
+      _controller.repeat();
+    } else if (!_shouldRotate && _controller.isAnimating) {
+      _controller.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Widget iconWidget = Icon(
+      widget.icon,
+      size: widget.iconSize,
+      color: widget.iconColor,
+    );
+
+    if (_shouldRotate) {
+      iconWidget = RotationTransition(
+        turns: _controller,
+        child: iconWidget,
+      );
+    }
+
     return AppContainer(
       padding: const EdgeInsets.all(48),
       width: double.infinity,
+      color: widget.backgroundColor,
       child: Column(
         children: [
-          Icon(
-            icon,
-            size: iconSize,
-          ),
+          iconWidget,
           const SizedBox(height: 24),
           Text(
-            title,
+            widget.title,
             style: AppTypography.containerTitle,
             textAlign: TextAlign.center,
           ),
           const SizedBox(height: 4),
           Text(
-            description,
+            widget.description,
             style: AppTypography.containerBody,
             textAlign: TextAlign.center,
           ),
